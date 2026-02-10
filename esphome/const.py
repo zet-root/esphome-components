a99f75ca7102036544a3285f615a830fb7b01975
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2026-02-11 08:45:06 +1300

    Bump version to 2026.1.5

--- esphome/const.py
+++ esphome/const.py
@@ -4,7 +4,7 @@ from enum import Enum
 
 from esphome.enum import StrEnum
 
-__version__ = "2026.1.4"
+__version__ = "2026.1.5"
 
 ALLOWED_NAME_CHARS = "abcdefghijklmnopqrstuvwxyz0123456789-_"
 VALID_SUBSTITUTIONS_CHARACTERS = (
